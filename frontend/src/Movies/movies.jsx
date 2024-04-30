62bc73facf79f474f588ba65fcbb659b852ec135
Utkarsh Raj <uraj33175@gmail.com>
2024-04-30 12:58:13 +0530

    css

--- frontend/src/Movies/movies.jsx
+++ frontend/src/Movies/movies.jsx
@@ -16,7 +16,6 @@ const Movies = () => {
           "https://movies-filter-zmla.onrender.com/"
         );
         setMovies(response.data);
-        console.log(response.data);
       } catch (error) {
         console.error("Error fetching movies:", error);
       }
@@ -28,7 +27,8 @@ const Movies = () => {
   const filterMovies = movies.filter(
     (movie) =>
       movie.movietitle.toLowerCase().includes(search.toLowerCase()) &&
-      (languageFilter === "" || movie.movielanguages.includes(languageFilter)) &&
+      (languageFilter === "" ||
+        movie.movielanguages.includes(languageFilter)) &&
       (countryFilter === "" || movie.moviecountries.includes(countryFilter)) &&
       (genreFilter === "" || movie.moviegenres.includes(genreFilter))
   );
@@ -62,10 +62,10 @@ const Movies = () => {
     "United States",
   ];
   const genres = [
-    "Action", 
-    "Adventure", 
-    "Fantasy", 
-    "Sport", 
+    "Action",
+    "Adventure",
+    "Fantasy",
+    "Sport",
     "Comedy",
     "Drama",
     "Romance",
@@ -76,103 +76,115 @@ const Movies = () => {
     "Sci-Fi",
     "Crime",
     "Biography",
-    "Documentary"
+    "Documentary",
   ];
 
   return (
     <div>
       <Navbar />
       <div>
-        <h1>Movies</h1>
+        <h1 className="text-3xl font-bold mb-4">Movies</h1>
       </div>
 
-      <div className="flex flex-col items-center mb-4 " >
-       <div className="pb-2">
-        <label className="relative block">
-          <span className="sr-only">Search</span>
-          <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
-              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-            </svg>
-          </span>
-          <input
-            onChange={handleSearch}
-            value={search}
-            className="placeholder:italic placeholder-text-slate-400 block bg-white w-full sm:w-64 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 text-sm"
-            placeholder="Search for city..."
-            type="text"
-            name="search"
-          />
-        </label>
-      </div>
-      </div>
-      <div className="border border-white mb-4">
-      <div className="grid grid-cols-7 gap-2">
-        <div className="border mr-2 border-black">
-          <label>Language:</label>
-          <select
-            value={languageFilter}
-            onChange={(e) => setLanguageFilter(e.target.value)}
-          >
-            <option value="">All</option>
-            {languages.map((language) => (
-              <option key={language} value={language}>
-                {language}
-              </option>
-            ))}
-          </select>
+      <div className="flex flex-col md:flex-row items-center mb-4">
+        <div className="pb-2 md:mr-4">
+          <label className="relative block">
+            <span className="sr-only">Search</span>
+            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                />
+              </svg>
+            </span>
+            <input
+              onChange={handleSearch}
+              value={search}
+              className="placeholder:italic placeholder-text-slate-400 block bg-white w-full sm:w-64 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 text-sm"
+              placeholder="Search for movie..."
+              type="text"
+              name="search"
+            />
+          </label>
         </div>
-        <div className="border mr-2 border-black">
-          <label>Country:</label>
-          <select
-            value={countryFilter}
-            onChange={(e) => setCountryFilter(e.target.value)}
-          >
-            <option value="">All</option>
-            {countries.map((country) => (
-              <option key={country} value={country}>
-                {country}
-              </option>
-            ))}
-          </select>
+        <div className="pb-2 md:mr-4">
+          <label className="relative block">
+            <span className="sr-only">Language</span>
+            <select
+              value={languageFilter}
+              onChange={(e) => setLanguageFilter(e.target.value)}
+              className="block bg-white border border-slate-300 rounded-md py-2 pl-3 pr-10 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 text-sm"
+            >
+              <option value="">All Languages</option>
+              {languages.map((language) => (
+                <option key={language} value={language}>
+                  {language}
+                </option>
+              ))}
+            </select>
+          </label>
         </div>
-        <div className="border mr-2 border-black">
-          <label>Genre:</label>
-          <select
-            value={genreFilter}
-            onChange={(e) => setGenreFilter(e.target.value)}
-          >
-            <option value="">All</option>
-            {genres.map((genre) => (
-              <option key={genre} value={genre}>
-                {genre}
-              </option>
-            ))}
-          </select>
+        <div className="pb-2 md:mr-4">
+          <label className="relative block">
+            <span className="sr-only">Country</span>
+            <select
+              value={countryFilter}
+              onChange={(e) => setCountryFilter(e.target.value)}
+              className="block bg-white border border-slate-300 rounded-md py-2 pl-3 pr-10 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 text-sm"
+            >
+              <option value="">All Countries</option>
+              {countries.map((country) => (
+                <option key={country} value={country}>
+                  {country}
+                </option>
+              ))}
+            </select>
+          </label>
         </div>
-      </div>
+        <div className="pb-2">
+          <label className="relative block">
+            <span className="sr-only">Genre</span>
+            <select
+              value={genreFilter}
+              onChange={(e) => setGenreFilter(e.target.value)}
+              className="block bg-white border border-slate-300 rounded-md py-2 pl-3 pr-10 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 text-sm"
+            >
+              <option value="">All Genres</option>
+              {genres.map((genre) => (
+                <option key={genre} value={genre}>
+                  {genre}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
       </div>
 
-      
-
-      <div className=" ">
-        <ul>
-        <div className="grid grid-cols-4 gap-4">
+      <div className=" bg-slate-300 ">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
           {filterMovies.map((movie) => (
-            
             <li key={movie._id}>
-              
+              <div className="shadow-2xl shadow-black border border-4 border-slate-500 rounded-xl scale-75 cursor-pointer " >
               <img
-                className="shadow-2xl border border-4 border-black rounded-xl"
+                
                 src={movie.moviemainphotos[0]}
                 alt={movie.movietitle}
-                style={{ width: "400px", height: "500px" }}
+                style={{ width: "100%", height: "auto" }}
               />
-              <h2 className="font-bold underline">{movie.movietitle}</h2>
+              
+              <h2 className="font-bold underline mt-2">{movie.movietitle}</h2>
+              </div>
             </li>
-            
           ))}
-          </div>
         </ul>
       </div>
     </div>
--- frontend/src/Movies/navbar.jsx
+++ frontend/src/Movies/navbar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
- const  Navbar = ()=>{
-    return (
-        <>
-        <div className="text-center py-4 bg-gradient-to-b bg-white sticky top-0 z-50 shadow-2xl bg-gradient-to-br  from-indigo-200  via-purple-200 to-pink-100 border border-black">
-            Navbar
-        </div>
-        </>
-    )
-}
+const Navbar = () => {
+  return (
+    <>
+      <div className="text-center py-2 sm:py-4 bg-gradient-to-b bg-white sticky top-0 z-50 shadow-2xl bg-gradient-to-br  from-indigo-200  via-purple-200 to-pink-100 border border-black">
+        Navbar
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
